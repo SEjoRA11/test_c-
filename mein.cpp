3ab13ee790f02c296dc84798a8e2b342738e54a2
SEjoRA11 <alt_ser1607@mail.ru>
2025-06-18 16:18:28 +0700

    list

--- mein.cpp
+++ mein.cpp
@@ -1,94 +1,106 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-
 using namespace std;
+template <typename T> 
+struct Node
+{
+    T data;
+    Node * next;
+    Node(const T&value) : data(value), next(nullptr){}
+};
 
-
-class Point{
+template <typename T>
+class LinkedList {
+private:
+    Node<T>* head; //указатель на начало списка
+    Node<T>* tail; //Указатель на конец списка
+    int size; // Размер списка
+    
 public:
+    LinkedList() : head(nullptr), tail(nullptr), size(0){}// конструктор по умолчанию
+    ~LinkedList(){ // деструктор для освобожления памяти
+        Node<T>*current = head;
+        while(current!=nullptr){
+            Node<T>* next = current->next;
+            delete current;
+            current = next;
+        }
+        head=tail=nullptr;
+        size = 0;
+    }
 
+    //Добовление элемента в начало списка
 
-Point(){
-    x=y=z=0;
-}
-Point(int x, int y, int z){
-    this ->x = x;
-    this ->y = y;
-    this ->z = z;
-}
-void print(){
-    cout<<"X: " << x << "\tY: " << y << "\tZ: " << z << endl;
-}
+    void push_front(const T& value){
+        Node<T>* newNode = new Node<T>(value);
+        if (head == nullptr){
+            head = tail = newNode;
+        }
+        else{
+            newNode->next = head;
+            head = newNode;
+        }
+        size++;
+    }
 
-int x;
-int y;
-int z;
-public:
+    //Добавление элемента в конец списка
+    void push_back(const T& value){
+        Node<T>*newNode = new Node<T>(value);
+        if (tail == nullptr){
+            head = tail = newNode;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
 
+    //Удаление элемента из начала списка
+    void pop_front(){
+        if(head == nullptr){
+            return;//список пуст
+        }
+        Node<T>* temp = head;
+        head = head->next;
+        delete temp;
+        if (head == nullptr){
+            tail = nullptr; //список стал пустым
+        }
+        size--;
+    }
 
+    //Получение значения элемента то индексу
+    T get_at(int index){
+        if (index < 0 || index >=size){
+            throw std::out_of_range("Index out of bounds");
+        }
+        Node<T>* current = head;
+        for (int i = 0; i < index; ++i){
+            current = current->next;
+        }
+        return current->data;
+    }
+    //Полуяение размера списка
+    int get_size() const{
+        return size;
+    }
 };
 
 int main(){
     setlocale(0, "ru");
+    LinkedList<int> list;
+    list.push_back(10);
+    list.push_back(20);
+    list.push_front(5);
 
-    string path = "myFile.txt";
-    // ifstream fin;
-    // fin.open(path);
-    // if (!fin.is_open())
-    // {
-    //     cout << "Ошибка открытия файла!";
-    // }
-    // else{
-    //     cout << "Файл открылся!" <<endl;
-    //     string str;
-    //     while(!fin.eof()){
-    //         str = "";
-    //         getline(fin, str);
-    //         cout << str << endl;;
-    //     }
-    // }
-    // fin.close();
+    cout<<"list size: " << list.get_size()<<'\n';
+    cout << "element at index 1 :" << list.get_at(1) << '\n';
 
-//fout.open("myFile.txt", ofstream::app);
-    // if(!fout.is_open()){
-    //     cout << "ошибка открытия файла!" << endl;
-    // }
-    // else{
-    //     fout <<"это наши данные" << endl;
-    //     fout<<555<<endl;;;;
-    // }
-    // fout.close();
+    list.pop_front();
+    cout<<"list size: " << list.get_size()<<'\n';
+    cout << "element at index 1 :" << list.get_at(1) << '\n';    
 
-    // Point a(23, 12, 1);
-    // Point b(12, 3, 2);
-    // ofstream fout;
-    // fout.open(path, ofstream::app);
-    // if (!fout.is_open()){
-    //     cout << "Ошибка открытия файла!" << endl;
-    //     return 1;
-    // }
-    // else{
-    //     cout<<"Файл открылся!"<< endl;
-    //     fout.write((char*)&b, sizeof(b));
-    // }
-    ifstream fin;
-    fin.open(path);
-
-    if (!fin.is_open()){
-        cout << "Ошибка открытия файла!" << endl;
-        return 1;
-    }
-    else{
-         cout<<"Файл открылся!"<< endl;
-        Point prt;
-        while(fin.read((char*)&prt, sizeof(Point))){
-            prt.print();
-        }
-
-    }
-
-    fin.close();
     return 0;
 } 
 
